02ea19ecf7ed87998fee2225713f23bbab1a5d6a
Diego Ortiz Gamboa <diegofergamboa@Diegos-MacBook-Pro.local>
2024-04-11 13:35:22 -0500

    Begin with exercises

--- courses.py
+++ courses.py
@@ -1,15 +1,22 @@
-points = { 
-    'A+' : 4.0,
-    'A' : 4.0,
-    'A-' : 3.67,
-    'B+' : 3.33, 
-    'B' :3.0, 
-    'B-' :2.67,
-    'C+' : 2.33, 
-    'C' :2.0, 
-    'C-' :1.67, 
-    'D+' :1.33, 
-    'D' :1.0, 
-    'F' :0.0
-}
+# Example 1:
+
+# Input: nums = [2,5,1,3,4,7], n = 3
+# Output: [2,3,5,4,1,7] 
+# Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+# Example 2:
+
+# Input: nums = [1,2,3,4,4,3,2,1], n = 4
+# Output: [1,4,2,3,3,2,4,1]
+# Example 3:
+
+# Input: nums = [1,1,2,2], n = 2
+# Output: [1,2,1,2]
+
+def findWordsContaining(words, x):
+
+
+examplewords = ['leet', 'code']
+examplen = 'e'
+result = findWordsContaining(examplewords, examplen)
+print(result)
 
