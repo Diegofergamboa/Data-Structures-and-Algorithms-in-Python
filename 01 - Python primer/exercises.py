233cbb22830e29bcf9add90401f076016a793ffd
Diego Ortiz Gamboa <diegofergamboa@Diegos-MacBook-Pro.local>
2024-04-26 11:11:09 -0500

    Exercise 22

--- 01 - Python primer/exercises.py
+++ 01 - Python primer/exercises.py
@@ -297,5 +297,8 @@ except EOFError:
 # r-1.22 Write a short Python program that takes two arrays a and b of length n storing int values, and returns the dot product of a and b. That is, it returns an array c of length n such that c[i] = a[i]·b[i], for i = 0,...,n−1.
 
 def doct_constructor(a,b):
+    c = []
+    index = 0
     if isinstance(a, list) and isinstance(b, list):
-        ''
+        while index <= 0:
+            a[index]
